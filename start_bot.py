27ed9623bf56d731d8208f3685420bdb1dd1f645
XcenaX <vlad-057@mail.ru>
2021-05-20 21:29:25 +0600

    update bd

--- start_bot.py
+++ start_bot.py
@@ -5,7 +5,7 @@ from pathlib import Path
 BASE_DIR = ""
 
 TELEGRAM = {
-    'bot_token': '1895297659:AAFdRK2iPvGAQeOCB8auCqOshuE3VKomenQ',
+    'bot_token': '1895297659:AAFRKuWeovfOtshIsTNdd8tWDREtDUG8Gwo',
     'channel_name': 'oceanswavenews',
     "BASE_DIR": BASE_DIR,
 }
