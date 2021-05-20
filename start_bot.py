59721a91662e603d20e90fbc47206de90a8a5220
XcenaX <vlad-057@mail.ru>
2021-05-20 17:47:54 +0600

    update

--- start_bot.py
+++ start_bot.py
@@ -1,5 +1,13 @@
 from Bot import Bot
-from oceanswave.settings import TELEGRAM
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+TELEGRAM = {
+    'bot_token': '1895297659:AAFdRK2iPvGAQeOCB8auCqOshuE3VKomenQ',
+    'channel_name': 'oceanswavenews',
+    "BASE_DIR": BASE_DIR,
+}
 
 TELEGRAM_BOT = Bot(TELEGRAM)
 TELEGRAM_BOT.start_bot()
